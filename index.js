263d66dda4531fd6532f2d1b46f1dfc36425de92
abdoel <alwanhasmadi@gmail.com>
2019-12-17 19:09:19 +0800

    updagte

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 $(document).ready(function() {
   $(".flex-container div").click(function() {
     $(".flex-container div").removeClass("focus");
-    $(".flex-container div img.close").removeClass("show");
+    $(".flex-container div img").removeClass("show");
     $(this).addClass("focus");
-
     $(this)
       .children()
       .addClass("show");
-
-    console.log(this);
   });
 
-  $(".flex-container div img.close").click(function(e) {
+  $(".close").click(function(e) {
     e.stopPropagation();
-
-    $(this).removeClass("show");
+    $(".flex-container div img").removeClass("show");
     $(".flex-container div").addClass("no-pointer-events");
-    $(".flex-container div.bg-size").removeClass("focus");
+    $(".flex-container div").removeClass("focus");
 
     setTimeout(function() {
       $(".flex-container div").removeClass("no-pointer-events");
